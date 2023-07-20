7ee8e3157c7e0d15f07588b35ea5598dad490548
MahmoudEasa <mu01011422865@gmail.com>
2023-07-20 22:58:44 +0000

    2-selection_sort.c 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,18 +12,24 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
+	int swap = 0;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
+	{
+		swap = 0;
 		for (j = i; j < size; j++)
 			if (array[i] > array[j])
 			{
 				temp = array[j];
 				array[j] = array[i];
 				array[i] = temp;
-				print_array(array, size);
+				swap = 1;
 			}
+		if (swap)
+			print_array(array, size);
+	}
 }
 
