3cdd3b972c0302aab6f20c4cb43961c3f1fc238e
MahmoudEasa <mu01011422865@gmail.com>
2023-07-20 22:08:55 +0000

    Fix 0-bubble_sort.c 5

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "sort.h"
 
 /**
@@ -15,24 +16,20 @@ void bubble_sort(int *array, size_t size)
 	if (array)
 	{
 		if (size < 2)
-		{
 			return;
-		}
-		else
+
+		while (!sorted)
 		{
-			while (!sorted)
+			sorted = 1;
+			for (i = 0; i < size - 1; i++)
 			{
-				sorted = 1;
-				for (i = 0; i < size - 1; i++)
+				if (array[i] > array[i + 1])
 				{
-					if (array[i] > array[i + 1])
-					{
-						temp = array[i];
-						array[i] = array[i + 1];
-						array[i + 1] = temp;
-						sorted = 0;
-						print_array(array, size);
-					}
+					temp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
+					sorted = 0;
+					print_array(array, size);
 				}
 			}
 		}
