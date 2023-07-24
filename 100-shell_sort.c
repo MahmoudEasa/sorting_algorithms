25116eb8b2eb9ae50a4e56683aa7bada23b33a08
MahmoudEasa <mu01011422865@gmail.com>
2023-07-24 19:11:51 +0000

    Fix 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,28 +12,20 @@
 
 void shell_sort(int *array, size_t size)
 {
-	int temp, *knuth, sorted = 0;
-	size_t i, j, k;
-	(void)knuth;
+	int temp, swap = 0;
+	size_t i, j, h = 1;
 
-	if (size <= 2)
+	if (size < 2)
 		return;
-	/*knuth = (int *)malloc(sizeof(int) * (size + 1));*/
-	/*if (!knuth)*/
-		/*exit(1);*/
 
-	/*knuth[0] = 1;*/
-	/*for (i = 1; i <= size; i++)*/
-		/*knuth[i] = (knuth[i - 1] * 3) + 1;*/
+	while (h <= size / 3)
+		h = (h * 3) + 1;
 
-	k = size / 2;
-	while (!sorted)
+	while (h > 0)
 	{
-		sorted = 1;
-		j = k;
-		for (i = 0; i < size && k > 0; i++)
+		for (i = 0; i < size; i++)
 		{
-			j = k + i;
+			j = h + i;
 			while (j < size)
 			{
 				if (array[i] > array[j])
@@ -41,15 +33,18 @@ void shell_sort(int *array, size_t size)
 					temp = array[j];
 					array[j] = array[i];
 					array[i] = temp;
-					sorted = 0;
-					print_array(array, size);
+					swap = 1;
 				}
-				j += k;
+				j += h;
 			}
 		}
-		if (k % 2 != 0)
-			k--;
-		k = k / 2;
+		if (swap)
+		{
+			print_array(array, size);
+			swap = 0;
+		}
+
+		h = h / 3;
 	}
 }
 
